f3e754cf3839d485541701237ee89ef94cafdba0
mewkill <lmjo091122@upemor.edu.mx>
2015-07-20 15:01:07 -0500

    Avance
    Proyecto Biblioteca

---
+++ Proyecto_Biblioteca/LoginView.m
@@ -0,0 +1,23 @@
+//
+//  LoginView.m
+//  Proyecto_Biblioteca
+//
+//  Created by JALOPEZ on 20/07/15.
+//  Copyright (c) 2015 JALOPEZ. All rights reserved.
+//
+
+#import "LoginView.h"
+
+@implementation LoginView
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
---
+++ Proyecto_Biblioteca/UserRegisterView.h
@@ -0,0 +1,13 @@
+//
+//  UserRegisterView.h
+//  Proyecto_Biblioteca
+//
+//  Created by JALOPEZ on 20/07/15.
+//  Copyright (c) 2015 JALOPEZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UserRegisterView : UIViewController
+
+@end
