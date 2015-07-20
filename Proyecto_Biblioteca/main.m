78d7a752ad5313e1c822982573625ffae65e9a20
mewkill <lmjo091122@upemor.edu.mx>
2015-07-20 12:52:34 -0500

    Initial Commit

---
+++ Proyecto_Biblioteca/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Proyecto_Biblioteca
+//
+//  Created by JALOPEZ on 20/07/15.
+//  Copyright (c) 2015 JALOPEZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
